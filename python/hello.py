68e5683f2cffae91f3b49ee9854d16025cbc5fd8
qadevtips <thecomputergoodguy@gmail.com>
2025-12-29 07:37:38 -0500

    Add initial Python script with greeting messages

---
+++ python/hello.py
@@ -0,0 +1,8 @@
+print("Hello, Python!")
+print("This is my first Python file in VS Code.")
+
+name = input("What is your name? ")
+print("Nice to meet you,", name)
+
+print("Python is ready for QA and development!")
+
